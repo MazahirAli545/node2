b35fab3fcde2950682003c8a34438e7f03eddf23
MazahirAli545 <mazahiralii768@gmail.com>
2025-03-10 12:54:37 +0530

    Adding DirectoyController.js

---
+++ src/controllers/Directory.controller.js
@@ -0,0 +1,30 @@
+import { PrismaClient } from "@prisma/client";
+import express from "express";
+
+const app = express();
+const prisma = new PrismaClient();
+
+async function DirectoryController(req, res) {
+  try {
+    const directory = await prisma.peopleRegistry.findMany({});
+
+    const Directory = directory.map((item) => {
+      return item.PR_UNIQUE_ID.split("-")[3] === "001";
+    });
+
+    return res.status(200).json({
+      message: "Directory fetched successfully",
+      success: true,
+      Directory,
+    });
+  } catch (error) {
+    console.error("Error fetching Directory:", error);
+    return res.status(500).json({
+      message: "Error fetching Directory",
+      success: false,
+      error: error.message,
+    });
+  }
+}
+
+export default DirectoryController;
