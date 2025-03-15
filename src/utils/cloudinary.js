978979b6c9e20655ab2486a5661dff6c3609bae4
MazahirAli545 <mazahiralii768@gmail.com>
2025-03-15 11:35:57 +0530

    store image changes21

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,9 +1,11 @@
 import multer from "multer";
 import dotenv from "dotenv";
-import { cloudinary } from "cloudinary";
+import pkg from "cloudinary";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
 
 dotenv.config();
+
+const { v2: cloudinary } = pkg;
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
