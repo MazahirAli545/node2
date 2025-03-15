e09ab8c10592290e54783dfe5db96f8bfd7c89b5
MazahirAli545 <mazahiralii768@gmail.com>
2025-03-15 09:40:31 +0530

    store image changes5

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -27,10 +27,8 @@ import multer from "multer";
 import path from "path";
 import fs from "fs";
 
-// Define the upload directory
-const uploadDir = path.join("src", "uploads");
-
-// Check if the directory exists, if not, create it
+// Ensure the uploads directory exists
+const uploadDir = "src/uploads";
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir, { recursive: true });
 }
@@ -49,7 +47,7 @@ const fileFilter = (req, file, cb) => {
   if (allowedTypes.includes(file.mimetype)) {
     cb(null, true);
   } else {
-    cb(new Error("Only JPEG, PNG, JPG FORMATS ARE ALLOWED"), false);
+    cb(new Error("Only JPEG, PNG, JPG formats are allowed"), false);
   }
 };
 
