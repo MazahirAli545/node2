fca6f7f9539ae0f77cc66f0df4f8939a7a75f833
MazahirAli545 <mazahiralii768@gmail.com>
2025-04-28 10:18:06 +0530

    Solve DB error

--- src/db/prismaClient.js
+++ src/db/prismaClient.js
@@ -1,20 +1,33 @@
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis; // Use globalThis to avoid issues in some environments
+// const globalForPrisma = globalThis; // Use globalThis to avoid issues in some environments
 
-let prisma;
+// let prisma;
 
-if (!globalForPrisma.prisma) {
-  globalForPrisma.prisma = new PrismaClient();
-}
-prisma = globalForPrisma.prisma;
-
-export default prisma;
-
-// const prisma = globalForPrisma.prisma || new PrismaClient();
-
-// if (process.env.NODE_ENV !== "production") {
-// globalForPrisma.prisma = prisma;
+// if (!globalForPrisma.prisma) {
+//   globalForPrisma.prisma = new PrismaClient();
 // }
+// prisma = globalForPrisma.prisma;
 
 // export default prisma;
+
+// // const prisma = globalForPrisma.prisma || new PrismaClient();
+
+// // if (process.env.NODE_ENV !== "production") {
+// // globalForPrisma.prisma = prisma;
+// // }
+
+// // export default prisma;
+
+//////////////////////////////////////////////////////////////////
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = globalThis;
+
+const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
+
+export default prisma;
--- src/index.js
+++ src/index.js
@@ -20,6 +20,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use("/api/user", userRouter);
 
+// console.log("232", userRouter);
+
 app.listen(PORT, () => {
   console.log("Server listening on port", PORT);
 });
