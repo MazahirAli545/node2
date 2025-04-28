8364b25ad9d5f6765461cda42b0b87d9e737914d
MazahirAli545 <mazahiralii768@gmail.com>
2025-04-28 10:25:02 +0530

    Solve DB error2

--- src/db/prismaClient.js
+++ src/db/prismaClient.js
@@ -24,10 +24,11 @@ import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis;
 
-const prisma = globalForPrisma.prisma ?? new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
+// Only create ONE instance
+if (!globalForPrisma.prisma) {
+  globalForPrisma.prisma = new PrismaClient();
 }
+
+const prisma = globalForPrisma.prisma;
 
 export default prisma;
