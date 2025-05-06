16dcf87beeea111f4afedbaba7ebbfae5c385b67
MazahirAli545 <mazahiralii768@gmail.com>
2025-05-06 15:47:29 +0530

    Adding Changes in Payment3

--- src/controllers/Payment.controller.js
+++ src/controllers/Payment.controller.js
@@ -1,3 +1,61 @@
+// import { PrismaClient } from "@prisma/client";
+// import prisma from "../db/prismaClient.js";
+// import express from "express";
+// import axios from "axios";
+
+// // const express = require("express");
+// const router = express.Router();
+// // const axios = require("axios");
+
+// const RAZORPAY_API_KEY = process.env.RAZORPAY_API_KEY ;
+// const RAZORPAY_SECRET_KEY = process.env.RAZORPAY_SECRET_KEY;
+
+// // router.post("/capture")
+// export const capturePayment = async (req, res) => {
+//   const { paymentId, amount } = req.body;
+
+//   try {
+//     const response = await axios({
+//       method: "POST",
+//       url: `https://api.razorpay.com/v1/payments/${paymentId}/capture`,
+//       auth: {
+//         username: RAZORPAY_API_KEY,
+//         Password: RAZORPAY_SECRET_KEY,
+//       },
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       data: {
+//         amount: amount * 100,
+//         currency: "INR",
+//       },
+//     });
+//     const paymentData = response.data;
+
+//     const savedPayment = await prisma.donationPayment.create({
+//       data: {
+//         paymentId: paymentData.id,
+//         amount: paymentData.amount,
+//         currency: paymentData.currency,
+//         status: paymentData.status,
+//         method: paymentData.method,
+//         captured: paymentData.captured,
+//       },
+//     });
+//     res.status(200).json({
+//       success: true,
+//       message: "Payment captured successfullt",
+//       data: savedPayment,
+//     });
+//   } catch (error) {
+//     console.log(error.response?.data || error.message);
+//     res.status(500).json({
+//       success: false,
+//       error: error.response?.data || "Payment captured failed",
+//     });
+//   }
+// };
+
 import { PrismaClient } from "@prisma/client";
 import prisma from "../db/prismaClient.js";
 import express from "express";
@@ -56,8 +114,6 @@ export const capturePayment = async (req, res) => {
     const amountInRuppes = paymentData.amount / 100;
     const savedPayment = await prisma.donationPayment.create({
       data: {
-        PR_ID,
-        PR_FULL_NAME,
         paymentId: paymentData.id,
         amount: amountInRuppes,
         currency: paymentData.currency,
