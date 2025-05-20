cab5cba0260cda0fb55ce1d1a4afbc8d3f26ce94
MazahirAli545 <mazahiralii768@gmail.com>
2025-05-20 13:52:49 +0530

    change in PR_UNIQUE_ID13

--- src/controllers/utils/familyUtils.js
+++ src/controllers/utils/familyUtils.js
@@ -38,12 +38,21 @@ export const getNextFamilyNumber = async (stateCode, districtCode, cityId) => {
         PR_CITY_CODE: cityId,
       },
       orderBy: { PR_FAMILY_NO: "desc" },
+      select: {
+        PR_FAMILY_NO: true,
+      },
     });
 
-    if (lastUserInArea) {
-      // Parse family number as integer and increment
-      const lastFamilyNum = parseInt(lastUserInArea.PR_FAMILY_NO, 10);
-      return (lastFamilyNum + 1).toString().padStart(3, "0");
+    // if (lastUserInArea) {
+    //   // Parse family number as integer and increment
+    //   const lastFamilyNum = parseInt(lastUserInArea.PR_FAMILY_NO, 10);
+    //   return (lastFamilyNum + 1).toString().padStart(3, "0");
+    // }
+    if (lastUserInArea && lastFamily.PR_FAMILY_NO) {
+      const nextFamilyNo = (parseInt(lastFamily.PR_FAMILY_NO, 10) + 1)
+        .toString()
+        .padStart(3, "0");
+      return nextFamilyNo;
     }
 
     // If no existing users in this area, start with "001"
