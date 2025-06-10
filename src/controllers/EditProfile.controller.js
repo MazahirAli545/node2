a4ceec841995f4e04b7bd0f04ecebc0c60799d16
MazahirAli545 <mazahiralii768@gmail.com>
2025-06-10 18:01:38 +0530

    Adding PR changes 11

--- src/controllers/EditProfile.controller.js
+++ src/controllers/EditProfile.controller.js
@@ -492,6 +492,18 @@ async function EditProfile(req, res) {
     WHERE PR_UNIQUE_ID LIKE CONCAT(SUBSTRING_INDEX(${parentId} COLLATE utf8mb4_unicode_ci, '-', 3), '-%')
     GROUP BY family
   `;
+        console.log(
+          "Member result:",
+          memberResult,
+          `
+          SELECT
+      SUBSTRING_INDEX(SUBSTRING_INDEX(PR_UNIQUE_ID COLLATE utf8mb4_unicode_ci, '-', 3), '-', -1) AS family,
+      MAX(CAST(SUBSTRING_INDEX(PR_UNIQUE_ID COLLATE utf8mb4_unicode_ci, '-', -1) AS UNSIGNED)) AS max_member
+    FROM PEOPLE_REGISTRY
+    WHERE PR_UNIQUE_ID LIKE CONCAT(SUBSTRING_INDEX(${parentId} COLLATE utf8mb4_unicode_ci, '-', 3), '-%')
+    GROUP BY family
+        `
+        );
       } else {
         query = prisma.$queryRaw`
           SELECT PR_UNIQUE_ID FROM PEOPLE_REGISTRY
@@ -509,6 +521,20 @@ async function EditProfile(req, res) {
     AND PR_MOBILE_NO = ${existingProfile.PR_MOBILE_NO}
     GROUP BY family
   `;
+
+        console.log(
+          "Member result:",
+          memberResult,
+          `
+          SELECT
+      SUBSTRING_INDEX(SUBSTRING_INDEX(PR_UNIQUE_ID COLLATE utf8mb4_unicode_ci, '-', 3), '-', -1) AS family,
+      MAX(CAST(SUBSTRING_INDEX(PR_UNIQUE_ID COLLATE utf8mb4_unicode_ci, '-', -1) AS UNSIGNED)) AS max_member
+    FROM PEOPLE_REGISTRY
+    WHERE PR_UNIQUE_ID LIKE CONCAT(${prefix}, '-%')
+    AND PR_MOBILE_NO = ${existingProfile.PR_MOBILE_NO}
+    GROUP BY family
+        `
+        );
       }
 
       const existing = await query;
@@ -561,18 +587,18 @@ async function EditProfile(req, res) {
         //   GROUP BY family
         // `;
 
-        console.log(
-          "Member result:",
-          memberResult,
-          `
-          SELECT
-            SUBSTRING_INDEX(SUBSTRING_INDEX(PR_UNIQUE_ID, '-', 3), '-', -1) AS family,
-            MAX(CAST(SUBSTRING_INDEX(PR_UNIQUE_ID, '-', -1) AS UNSIGNED)) AS max_member
-          FROM PEOPLE_REGISTRY
-          WHERE PR_UNIQUE_ID LIKE CONCAT(SUBSTRING_INDEX(${prefix}, '-', 3), '-%')
-          GROUP BY family
-        `
-        );
+        // console.log(
+        //   "Member result:",
+        //   memberResult,
+        //   `
+        //   SELECT
+        //     SUBSTRING_INDEX(SUBSTRING_INDEX(PR_UNIQUE_ID, '-', 3), '-', -1) AS family,
+        //     MAX(CAST(SUBSTRING_INDEX(PR_UNIQUE_ID, '-', -1) AS UNSIGNED)) AS max_member
+        //   FROM PEOPLE_REGISTRY
+        //   WHERE PR_UNIQUE_ID LIKE CONCAT(SUBSTRING_INDEX(${prefix}, '-', 3), '-%')
+        //   GROUP BY family
+        // `
+        // );
 
         familyNumber = memberResult[0].family;
         const nextMember = Number(memberResult[0].max_member) + 1;
