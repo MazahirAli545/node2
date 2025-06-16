b0b4c8a4fcd5ee331f7ebbf1d77e6880ce75c838
MazahirAli545 <mazahiralii768@gmail.com>
2025-06-16 18:02:14 +0530

    16/06/25 changes in events Notification3

--- src/controllers/utils/EventsNotification.controller.js
+++ src/controllers/utils/EventsNotification.controller.js
@@ -1,7 +1,7 @@
 // utils/notificationHandler.js
 import { GoogleAuth } from "google-auth-library";
 import axios from "axios";
-import prisma from "../../db/prismaClient";
+import prisma from "../../db/prismaClient.js";
 
 const serviceAccount = {
   type: process.env.FIREBASE_TYPE,
