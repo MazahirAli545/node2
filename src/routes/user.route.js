fe333840795f4c01e18213f9263726144d8b1580
MazahirAli545 <mazahiralii768@gmail.com>
2025-06-30 11:46:50 +0530

    30/06/25 Adding UserNotification 4

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -132,7 +132,7 @@ const userRouter = Router();
 
 // userRouter.post("/register", registeredfcmToken);
 // userRouter.post("/remove", removeFcmToken);
-userRouter.get("/user/combined", getUserEventsAndPayments);
+userRouter.get("/combined", getUserEventsAndPayments);
 userRouter.post("/logout", verifyToken, logoutUser);
 userRouter.put("/language", updateLanguage);
 userRouter.get("/language", getLanguage);
